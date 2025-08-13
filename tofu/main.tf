e01bc5999d83c2401d9664f0878ccfed0e303faa
ForestMars <themarsgroup+gh@gmail.com>
2025-08-13 17:17:54 -0400

    tests fixed

--- tofu/main.tf
+++ tofu/main.tf
@@ -1,3 +1,12 @@
-terraform { required_version = ">= 1.0" }
-resource "local_file" "hello" { filename = "/hello.txt" content = "Hello, World from OpenTofu!" }
-output "hello_content" { value = local_file.hello.content }
+terraform {
+  required_version = ">= 1.0"
+}
+
+resource "local_file" "hello" {
+  filename = "${path.module}/hello.txt"
+  content = "Hello, World from OpenTofu!"
+}
+
+output "hello_content" {
+  value = local_file.hello.content
+}
